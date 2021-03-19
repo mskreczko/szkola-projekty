7d98a791927c955de8ebbd3e7742f9206334033f
Michał Skreczko <mic.skreczko@gmail.com>
2021-03-19 10:30:21 +0100

    working quiz

--- django/quiz/urls.py
+++ django/quiz/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.render_quiz, name='render_quiz')
+    path('', views.render_quiz, name='render_quiz'),
+    path('evaluate/', views.render_score, name='render_score')
 ]
--- django/quiz/views.py
+++ django/quiz/views.py
@@ -4,3 +4,13 @@ from .models import Question
 def render_quiz(request):
     questions = Question.objects.all()
     return render(request, 'quiz/index.html', {'questions': questions})
+
+def render_score(request):
+    result = 0
+    correct_answers = dict(request.POST)['quiz_correct_answer']
+    for key in request.POST:
+       if len(key.split("|")) > 1:
+           question_number = int(key.split("|")[1])
+           if request.POST[key] == correct_answers[question_number-1].replace(" ", ""):
+               result += 1
+    return render(request, 'quiz/index.html', {'result': str(result)})
